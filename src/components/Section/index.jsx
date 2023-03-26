7bf1ec818f146bc5c48238c55d5113d0258e83ea
jpfagundes <jpsafagundes@hotmail.com>
2023-03-26 11:14:24 -0300

    adjusting carousel dependencies

--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -1,12 +1,12 @@
 import { Container } from "./styles";
-import Carousel from "react-elastic-carousel";
+import Carousel from "@itseasy21/react-elastic-carousel";
 
 export function Section({ title, children}) {
   const breakPoints = [
     { width: 1, itemsToShow: 1 },
     { width: 550, itemsToShow: 2 },
     { width: 910, itemsToShow: 3 },
-    { width: 1200, itemsToShow: 4 },
+    { width: 1140, itemsToShow: 4 },
   ];
 
   return(
